f14dbfeece231b16ef5042f10bcdeb7e3b2632da
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-01-27 23:30:37 +0000

    Working on flashcard css and layout

--- projects/website/public/flashCards.js
+++ projects/website/public/flashCards.js
@@ -2,4 +2,3 @@ import { setCanvas, drawFilledRect, width, height } from './graphics.js';
 const canvas = document.getElementById('screen')
 setCanvas(canvas);
 
-drawFilledRect(width/2, height/2, 100,100, 'black')
