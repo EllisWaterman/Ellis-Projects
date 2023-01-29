9e4f37ab07421921a26a9c45dc1d33c9409ff9b9
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-01-29 06:13:25 +0000

    flashcards

--- projects/website/public/flashCards.js
+++ projects/website/public/flashCards.js
@@ -1,4 +1,16 @@
-import { setCanvas, drawFilledRect, width, height } from './graphics.js';
+import {setCanvas} from './graphics.js';
 const canvas = document.getElementById('screen')
 setCanvas(canvas);
 
+
+
+
+const p1 = document.createElement('p')
+const div = document.createElement('div')
+div.append(document.createTextNode('Side 2'))
+
+div.setAttribute('class', 'flashcard_side1')
+div.append(p1)
+body.append(div)
+canvas.onclick= () =>{
+ }
