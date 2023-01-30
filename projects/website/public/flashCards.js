190cb31a1f458fefedb74ac124ccced509f73305
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-01-30 23:33:20 +0000

    Flashcards

--- projects/website/public/flashCards.js
+++ projects/website/public/flashCards.js
@@ -2,15 +2,6 @@ import {setCanvas} from './graphics.js';
 const canvas = document.getElementById('screen')
 setCanvas(canvas);
 
-
-
-
-const p1 = document.createElement('p')
-const div = document.createElement('div')
-div.append(document.createTextNode('Side 2'))
-
-div.setAttribute('class', 'flashcard_side1')
-div.append(p1)
-body.append(div)
 canvas.onclick= () =>{
- }
+    g.setAttribute('class', 'hidden')
+}
