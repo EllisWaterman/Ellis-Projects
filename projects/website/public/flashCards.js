424e67b96fd96e225ebc3832fd32783c58f53879
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-01-31 23:32:38 +0000

    Flashcards revamp

--- projects/website/public/flashCards.js
+++ projects/website/public/flashCards.js
@@ -1,14 +1,25 @@
-const card = document.getElementsByClassName('card')[0]
+const card = document.getElementsByClassName('card')[0];
+const div = document.createElement('div');
 
 card.onclick = (e) => {
-    Array.from(card.getElementsByTagName('div')).forEach(element => {
-        let classes = element.getAttribute('class').split(' ');
-        let index;
-        if ((index = classes.indexOf('hidden')) > -1) {
-            classes.splice(index, 1)
-        } else {
-            classes.push('hidden')
-        }
-        element.setAttribute('class', classes.join(' '))
-    });
+    let classesSide1 = card.querySelector('div')
+    let classesSide2 = card.querySelector('div.hidden')
+    console.log(classesSide1.outerHTML)
+    console.log(classesSide2.outerHTML)
+    if(classesSide1.outerHTML.indexOf('hidden') || classesSide2.outerHTML.indexOf('hidden')) {
+classesSide1.classList.add('hidden')
+classesSide2.classList.remove('hidden')
+    }
+    //  card.querySelectorAll('div').forEach(element => {
+    //     let classes = element.getAttribute('class').split(' ');
+    //     let index;
+    //     console.log(classes.outerHTML)
+
+    //     if ((index = classes.indexOf('hidden')) > -1) {
+    //         classes.classList.remove('hidden');
+    //     } else {
+    //         classes.classList.add('hidden')
+    //     }
+    //     element.setAttribute('class', classes.join(' '))
+    // });
 }
