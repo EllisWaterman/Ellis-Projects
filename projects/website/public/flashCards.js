7c7fe4f0f7123ee5eaa0c51836e41257e4a83975
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-02-01 05:19:42 +0000

    Rough Swap forwards between cards working.

--- projects/website/public/flashCards.js
+++ projects/website/public/flashCards.js
@@ -1,11 +1,28 @@
-const card = document.getElementsByClassName('card')[0];
+const cards = document.querySelector('.cards')
 
-card.onclick = (e) => {
-    card.querySelectorAll('div').forEach(element => {
+cards.onclick = (e) => {
+    let activeCard;
+    e.path.forEach(element => {
+        if (element.classList && element.classList.contains('card'))
+        activeCard = element
+    });
+    activeCard.querySelectorAll('div').forEach(element => {
         if (element.classList.contains('hidden')) {
             element.classList.remove('hidden');
         } else {
             element.classList.add('hidden')
         }
     });
+}
+
+const forward = document.querySelector('.forward')
+forward.onclick = (e) => {
+    let active = document.querySelector('.card.active')
+    active.classList.remove('active')
+    active.classList.add('hidden')
+    // find the active card
+    // make it not active
+active.nextElementSibling.classList.add('active')
+active.nextElementSibling.classList.remove('hidden')
+
 }
