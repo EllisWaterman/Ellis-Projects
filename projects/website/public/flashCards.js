ad21ec1e46cff10a4b85d31dd3b9b803fe02ecb8
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-02-02 23:31:18 +0000

    Backwards and Fowards Working properly. Flashcards working properly.

--- projects/website/public/flashCards.js
+++ projects/website/public/flashCards.js
@@ -1,12 +1,8 @@
 const cards = document.querySelector('.cards')
 
 cards.onclick = (e) => {
-    let activeCard;
-    e.path.forEach(element => {
-    
-    if (element.classList && element.classList.contains('card'))
-        activeCard = element
-    });
+    let activeCard = e.target.parentNode
+
     activeCard.querySelectorAll('div').forEach(element => {
         if (element.classList.contains('hidden')) {
             element.classList.remove('hidden');
@@ -19,20 +15,28 @@ cards.onclick = (e) => {
 const forward = document.querySelector('.forward')
 forward.onclick = (e) => {
     let active = document.querySelector('.card.active')
+    if(active.nextElementSibling === null) {
+        return 
+   } else {
     active.classList.remove('active')
     active.classList.add('hidden')
 active.nextElementSibling.classList.add('active')
 active.nextElementSibling.classList.remove('hidden')
-
+   }
 }
 const backward = document.querySelector('.backward')
 backward.onclick = (e) => {
+   
     let active = document.querySelector('.card.active')
+    if(active.previousElementSibling === null) {
+        return 
+   } else {
     active.classList.remove('active')
     active.classList.add('hidden')
+    
 active.previousElementSibling.classList.add('active')
 active.previousElementSibling.classList.remove('hidden')
-
+    }
 }
 
 
