d3173b09df9a24a62812e643429bb61482b869ab
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-02-03 23:32:49 +0000

    Fixed last major bug in flashcards

--- projects/website/public/flashCards.js
+++ projects/website/public/flashCards.js
@@ -2,7 +2,10 @@ const cards = document.querySelector('.cards')
 
 cards.onclick = (e) => {
     let activeCard = e.target.parentNode
-
+    console.log(activeCard)
+    if (activeCard.classList.contains('cards')) {
+        return
+    }
     activeCard.querySelectorAll('div').forEach(element => {
         if (element.classList.contains('hidden')) {
             element.classList.remove('hidden');
@@ -15,27 +18,27 @@ cards.onclick = (e) => {
 const forward = document.querySelector('.forward')
 forward.onclick = (e) => {
     let active = document.querySelector('.card.active')
-    if(active.nextElementSibling === null) {
-        return 
-   } else {
-    active.classList.remove('active')
-    active.classList.add('hidden')
-active.nextElementSibling.classList.add('active')
-active.nextElementSibling.classList.remove('hidden')
-   }
+    if (active.nextElementSibling === null) {
+        return
+    } else {
+        active.classList.remove('active')
+        active.classList.add('hidden')
+        active.nextElementSibling.classList.add('active')
+        active.nextElementSibling.classList.remove('hidden')
+    }
 }
 const backward = document.querySelector('.backward')
 backward.onclick = (e) => {
-   
+
     let active = document.querySelector('.card.active')
-    if(active.previousElementSibling === null) {
-        return 
-   } else {
-    active.classList.remove('active')
-    active.classList.add('hidden')
-    
-active.previousElementSibling.classList.add('active')
-active.previousElementSibling.classList.remove('hidden')
+    if (active.previousElementSibling === null) {
+        return
+    } else {
+        active.classList.remove('active')
+        active.classList.add('hidden')
+
+        active.previousElementSibling.classList.add('active')
+        active.previousElementSibling.classList.remove('hidden')
     }
 }
 
