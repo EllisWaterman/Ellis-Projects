08e2fee4486320ba7d3873fb20e2b727af48b9e2
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-02-18 10:09:18 -0800

    Updating assignments/refactor-tic-tac-toe/code.js

--- assignments/refactor-tic-tac-toe/code.js
+++ assignments/refactor-tic-tac-toe/code.js
@@ -82,8 +82,8 @@ registerOnclick((x, y) => {
 
   let winner = checkIfWinner()
 
-  r = Math.floor((y - boardTop) / cellSize);
-  c = Math.floor((x - boardLeft) / cellSize);
+  let r = Math.floor((y - boardTop) / cellSize);
+  let c = Math.floor((x - boardLeft) / cellSize);
 
   // Only do anything if it's a legal move and the game isn't over.
   if (winner === null && 0 <= r && r < 3 && 0 <= c && c < 3 && board[r][c] === '') {
