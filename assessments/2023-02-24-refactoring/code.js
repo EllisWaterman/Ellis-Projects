f56fb5f45e29e709b86146dd4ee9b87d3677eba9
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-02-26 16:43:40 -0800

    Updating assessments/2023-02-24-refactoring/code.js

--- assessments/2023-02-24-refactoring/code.js
+++ assessments/2023-02-24-refactoring/code.js
@@ -24,26 +24,26 @@ const buttSize = size * (buttP / total);
 
 const drawHead = (headY) => {
   const headRadius = headSize / 2
-  drawHeadCircle(headY,headRadius)
-  drawEyes(headRadius * 0.25,headY)
-  drawNose(headRadius * 0.8,headY)
-  drawMouth(headRadius,headY)
+  drawHeadCircle(headY, headRadius)
+  drawEyes(headRadius * 0.25, headY)
+  drawNose(headRadius * 0.8, headY)
+  drawMouth(headRadius, headY)
   drawHat(headY - headRadius * 0.9, headRadius * 2.25, headRadius * 1.25)
 }
-const drawHeadCircle = (headY,headRadius) => {
+const drawHeadCircle = (headY, headRadius) => {
   drawCircle(x, headY, headRadius + 2, 'black', 3);
   drawFilledCircle(x, headY, headRadius, 'white', 3);
 }
-const drawEyes = (eyeSpacing,headY) => {
+const drawEyes = (eyeSpacing, headY) => {
   drawFilledCircle(x - eyeSpacing, headY - eyeSpacing, 4, 'black');
   drawFilledCircle(x + eyeSpacing, headY - eyeSpacing, 4, 'black');
 }
 
 
-const drawNose = (noseLength,headY) => {
+const drawNose = (noseLength, headY) => {
   drawFilledTriangle(x, headY, x + noseLength, headY + noseLength * 0.2, x, headY + noseLength * 0.3, 'orange');
 }
-const drawMouth = (headRadius,headY) => {
+const drawMouth = (headRadius, headY) => {
   for (let i = 0; i < 5; i++) {
     const dy = -2 * (2.1 ** Math.abs(i - 2));
     drawFilledCircle(x - (i - 2.3) * headRadius * 0.21, headY + headRadius * 0.65 + dy, 4, 'black');
@@ -55,21 +55,21 @@ const drawHat = (brimTop, brimWidth, hatHeight) => {
   drawFilledRect(x - brimWidth / 2, brimTop, brimWidth, brimHeight, 'black');
   drawFilledRect(x - hatWidth / 2, brimTop - hatHeight, hatWidth, hatHeight, 'black');
 }
-const drawTorso = (headY) => {
+const drawTorso = (headY,torsoY) => {
   const torsoRadius = torsoSize / 2;
-  const torsoY = headY + headSize / 2 + torsoSize / 2;
-  drawTorsoCircle(x,torsoY,torsoRadius)
-  drawButtons(x,torsoY,torsoRadius)
-  drawArms(x + torsoRadius * 0.6, x + torsoRadius * 2.35,torsoY,torsoRadius);
+  
+  drawTorsoCircle(x, torsoY, torsoRadius)
+  drawButtons(x, torsoY, torsoRadius)
+  drawArms(x + torsoRadius * 0.6, x + torsoRadius * 2.35, torsoY, torsoRadius);
 
 }
-const drawTorsoCircle = (x,torsoY,torsoRadius) => {
+const drawTorsoCircle = (x, torsoY, torsoRadius) => {
   drawCircle(x, torsoY, torsoRadius + 2, 'black', 3);
   drawFilledCircle(x, torsoY, torsoRadius, 'white', 3);
 }
 
 
-const drawArms = (x1, x2,torsoY,torsoRadius) => {
+const drawArms = (x1, x2, torsoY, torsoRadius) => {
   drawLine(x1, torsoY - torsoRadius * 0.25, x2, torsoY - torsoRadius * 0.85, 'black', 3);
   x1 = x + torsoRadius * 0.6 * -1;
   x2 = x + torsoRadius * 2.35 * -1;
@@ -79,7 +79,7 @@ const drawArms = (x1, x2,torsoY,torsoRadius) => {
 
 
 
-const drawButtons = (x,torsoY,torsoRadius) => {
+const drawButtons = (x, torsoY, torsoRadius) => {
   for (let i = 0; i < 3; i++) {
     drawFilledCircle(x, torsoY - torsoRadius * 0.5 + i * torsoRadius * 0.5, 4, 'black');
   }
@@ -87,7 +87,7 @@ const drawButtons = (x,torsoY,torsoRadius) => {
 const drawButt = (torsoY) => {
   drawButtCircle(buttSize / 2, torsoY)
 }
-const drawButtCircle = (buttRadius,torsoY) => {
+const drawButtCircle = (buttRadius, torsoY) => {
   const buttY = torsoY + torsoSize / 2 + buttSize / 2;
   drawCircle(x, buttY, buttRadius + 2, 'black', 3);
   drawFilledCircle(x, buttY, buttRadius, 'white', 3);
@@ -96,10 +96,11 @@ const drawButtCircle = (buttRadius,torsoY) => {
 
 
 const drawSnowman = () => {
+  const torsoY = headY + headSize / 2 + torsoSize / 2;
   const headY = (base - size) + headSize / 2;
   drawHead(headY)
-  drawTorso(headY)
-drawButt(torsoY)
+  drawTorso(headY,torsoY)
+  drawButt(torsoY)
 }
 drawBackground()
 drawSnowman()
