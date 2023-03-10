3f89ee3de9c291c1efee83d3959adff9e098fb28
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-03-10 15:29:35 -0800

    Updating assessments/2023-03-10-refactoring/code.js

--- assessments/2023-03-10-refactoring/code.js
+++ assessments/2023-03-10-refactoring/code.js
@@ -71,7 +71,7 @@ const drawPicture = (horizon) => {
     let leavesX = treeBaseX + trunkWidth / 2;
     let leavesRadius = 40;
     let leavesY = treeBaseY - trunkHeight - (leavesRadius - 2);
-    drawTrunk(treeBaseX, treeBaseY, trunkWidth, trunkHeight)
+    drawTrunk(treeBaseX, treeBaseY, trunkHeight, trunkWidth)
     drawLeaves(leavesX, leavesY, leavesRadius)
     drawApples(leavesX, leavesY, 6, numApples)
   }
