d4bd6eb779c05e9cb5475f81a477c5936550f8d6
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-03-10 14:46:46 -0800

    Updating assessments/2023-03-10-refactoring/code.js

--- assessments/2023-03-10-refactoring/code.js
+++ assessments/2023-03-10-refactoring/code.js
@@ -35,7 +35,7 @@ const drawPicture = (horizon) => {
   }
 
 const drawSun = (sunSize) => {
-  drawSunRays(100,2,200,1)
+  drawSunRays(100,2,100,1)
   drawFilledCircle(width, 0, sunSize, 'yellow');
 }
 
