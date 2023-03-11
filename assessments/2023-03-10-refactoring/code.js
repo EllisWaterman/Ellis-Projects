afb4fbf83d9283520c28b01deb268953d2a527b3
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-03-11 09:53:09 -0800

    Updating assessments/2023-03-10-refactoring/code.js

--- assessments/2023-03-10-refactoring/code.js
+++ assessments/2023-03-10-refactoring/code.js
@@ -100,7 +100,7 @@ const drawBackground = (horizon) => {
 }
 
 const drawTrees = (horizon) => {
-  let numTrees = 10;
+  let numTrees = 12;
   let minApples = 5;
   let maxApples = 8;
   let gap = width / (numTrees + 1);
