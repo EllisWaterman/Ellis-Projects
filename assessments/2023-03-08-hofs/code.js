b0a387ae3fd805a19632a1ed0ee005ce9d8184f6
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-03-14 14:45:28 -0700

    Updating assessments/2023-03-08-hofs/code.js

--- assessments/2023-03-08-hofs/code.js
+++ assessments/2023-03-08-hofs/code.js
@@ -1,9 +1,17 @@
 const evens = (numbers) => {
   let returnArray = []
-  for(let i = 0; i < numbers.length; i++) {
-    if(numbers[i] % 2 === 0) {
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
       returnArray.push(numbers[i])
     }
+  }
+  return returnArray
+}
+
+const shouty = (strings) => {
+  let returnArray = []
+  for (let i = 0; i < strings.length; i++) {
+    returnArray.push(strings[i].toUpperCase())
   }
   return returnArray
 }
