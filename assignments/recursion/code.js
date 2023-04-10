b21ad9a96fd73e79718bb0855b0d5ba5812f0b02
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-10 15:28:31 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -17,7 +17,9 @@ const triangular = (n) => {
 const fibonacci = (n) => {
   if (n === 0) {
     return 0;
-  } else {
-    return  fibonacci(n - 1) + fibonacci(n-2)
+  } else if (n === 1) {
+    return 0;
+  }else {
+    return fibonacci(n - 1) + fibonacci(n - 2)
   }
 }
