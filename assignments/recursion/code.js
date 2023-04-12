6a27d2603c8af5ac14135bf65caac7bb2af491cb
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-12 14:54:17 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -44,7 +44,7 @@ const search = (n, number) => {
   if (n.length === 0) {
     return false
   } else {
-     if(n.indexOf(3) === number) {
+     if(n.indexOf() === number) {
        return true
      } else return false
   }
