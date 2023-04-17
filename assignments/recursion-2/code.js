322377c622156e471540ef702cbc62b4588ed857
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-17 14:57:33 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -2,6 +2,6 @@ const maximum = (n) => {
   if (n.length === 0) {
     return -Infinity
   } else {
-    return n[0] + Math.max(maximum(n.slice(1)))
+    return Math.max(n[0], maximum(n.slice(1)))
 }
 }
