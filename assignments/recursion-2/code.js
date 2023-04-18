4d32638c3277ca6684deea940f9fd0184a9aa147
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-18 14:54:05 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -14,13 +14,11 @@ const multiply = (a, b) => {
   }
 }
 const double = (a, b) => {
-  // if (b === 0) {
-  //   return 0
-  // } else {
-  //   return a * (2 * (double(a, b--)))
-  // }
-
-  return a * (2*b)
+  if (b === 0) {
+    return 0
+  } else {
+    return a * (2 * (double(a, b--)))
+  }
 }
 const maximum = (n) => {
   if (n.length === 0) {
