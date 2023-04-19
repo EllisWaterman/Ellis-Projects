8371abb62b5e9102da70b8d6b7dd2226a9ea07db
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-19 15:08:23 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -66,9 +66,9 @@ const countXs = (s) => {
 }
 
 const every = (n, predicate) => {
-  if (predicate(n[0])) {
+  if (n.length === 0) {
     return true
-  } else if (n.length === 0) {
+  } else if (predicate(n[0])) {
     return false
     } else {
     return every(n.slice(1), predicate)
