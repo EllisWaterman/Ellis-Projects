1ce7e475432073890c0aeba7a2f0c81d2c2a7398
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-19 15:09:32 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -69,7 +69,7 @@ const every = (n, predicate) => {
   if (n.length === 0) {
     return true
   } else if (predicate(n[0])) {
-    return true
+    return false
     } else {
     return every(n.slice(1), predicate)
   }
