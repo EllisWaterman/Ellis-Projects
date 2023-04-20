4b8a93c1e973f76f7e13cdfdc12693e3340f7625
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-20 15:20:29 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -46,6 +46,6 @@ const sumNested = (n) => {
   } else if (isNumber(n[0])) {
     return n[0] + sumNested(n.slice(1))
   } else {
-    return n[0] + sumNested(n.slice(1))
+    return sumNested(n.slice(1))
   }
 }
