db99544aa6f4cee3933c15cdeb2b0bd83cf4eadd
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-21 15:00:03 -0700

    Updating assessments/2023-04-21-recursion/code.js

--- assessments/2023-04-21-recursion/code.js
+++ assessments/2023-04-21-recursion/code.js
@@ -92,5 +92,5 @@ const nvwls = (s) => {
     return s
   } else if (s[0] === 'a' || s[0] === 'e' || s[0] === 'i' || s[0] === 'o' || s[0] === 'u') {
     return nvwls(s.slice(1))
-  }
+  } else return s
 }
