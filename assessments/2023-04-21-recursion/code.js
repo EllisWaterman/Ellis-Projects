58d56c07c7ca237e10abf0054cdb205314f5f7c5
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-21 14:57:58 -0700

    Updating assessments/2023-04-21-recursion/code.js

--- assessments/2023-04-21-recursion/code.js
+++ assessments/2023-04-21-recursion/code.js
@@ -90,7 +90,7 @@ const sumPrimesBelow = (n) => {
 const nvwls = (s) => {
   if(s.length === 0) {
     return s
-  } else {
-    return 
+  } else if (s[0] === 'a' || s[0] === 'e' || s[0] === 'i' || s[0] === 'o' || s[0] === 'u'){
+    return s[0] + nvwls()
   }
 }
