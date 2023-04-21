ccf4ab58ff6e9d04e6f17d7089048c630b795759
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-21 15:23:50 -0700

    Updating assessments/2023-04-21-recursion/code.js

--- assessments/2023-04-21-recursion/code.js
+++ assessments/2023-04-21-recursion/code.js
@@ -122,7 +122,7 @@ const map = (list, fn) => {
     return 0
   } else {
     newList.first = fn(newList.first)
-    newList.rest = fn(newList.rest)
+    newList.rest = fn(map(newList.rest,fn))
   }
   return newList
 }
