ec7e9ae90811f4397bc2d6ada8e9efdb0c1bdf83
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-21 14:45:44 -0700

    Updating assessments/2023-04-21-recursion/code.js

--- assessments/2023-04-21-recursion/code.js
+++ assessments/2023-04-21-recursion/code.js
@@ -60,7 +60,7 @@ const sumSquares = (n) => {
 
 const maximum = (n) => {
   if(n.length === 0) {
-    return -Infinty
+    return -Infinity
   } else {
     return Math.max(n[0], maximum(n.slice(1)))
   }
