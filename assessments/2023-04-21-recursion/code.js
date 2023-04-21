b36ec38e7f61d2d239cd139cf1f261af9034ebe1
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2023-04-21 14:39:02 -0700

    Updating assessments/2023-04-21-recursion/code.js

--- assessments/2023-04-21-recursion/code.js
+++ assessments/2023-04-21-recursion/code.js
@@ -43,7 +43,9 @@ const factorial = (n) => {
 const fibonacci = (n) => {
   if(n === 0) {
     return 0
-  } else {
+  } else if (n === 1) {
+    return 1
+    }else {
     return fibonacci(n-1) + fibonacci(n-2)
   }
 }
